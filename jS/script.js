b4044fa8f2a9b05176e9b359fa8e191cd2dd28b2
Halls06 <antoniomotoe4plus@gmail.com>
2024-11-28 01:05:55 -0300

    update at script

--- jS/script.js
+++ jS/script.js
@@ -1,18 +1,17 @@
 function toggleMode() {
-    const html = document.documentElement
-    html.classList.toggle('light')
+  const html = document.documentElement
+  html.classList.toggle("light")
 
-    const img = document.querySelector('#profile img')
+  const img = document.querySelector("#profile img")
 
-    if(html.classList.contains('light')) {
-        img.setAttribute('src', './assets/light-mode.png')
-    }else{
-        img.setAttribute('src', "./assets/tony450PX.png")
-    }
-    
+  if (html.classList.contains("light")) {
+      img.setAttribute("src", "./assets/tony450PX.png")
+    } else {
+      img.setAttribute("src", "./assets/light-mode.png")
+  }
 }
 
-    /*if(html.classList.contains('light')){
+/*if(html.classList.contains('light')){
 
         html.classList.remove('light')
     }else {
